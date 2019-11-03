fcdc025d1ea7ec143d961e5f0dff7b20a3360669
Stefan Dörig <sdoerig@bluewin.ch>
2019-11-03 22:11:30 +0100

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{error, web, FromRequest, HttpResponse, guard};
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
+use actix_web::middleware::Logger;
+use env_logger;
 
 mod user;
 use crate::user::{index,user,user_deserialize, user_deserialize_json};
@@ -16,8 +18,13 @@ pub fn main() {
     let data = AppState {
         count: Arc::new(AtomicUsize::new(0)),
     };
+    
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
 
     HttpServer::new(move || App::new()
+        .wrap(Logger::default())
+        .wrap(Logger::new("%a %{User-Agent}i"))
         .data(data.clone())
         .route("/async", web::to_async(index))
         .route("/users/{userid}/{friend}", // <- define path parameters
